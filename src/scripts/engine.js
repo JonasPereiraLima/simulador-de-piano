548c007ad7e19cb251f88e7c4fe8a312108e3b73
Jonas <jonas.pereira.lima1@gmail.com>
2023-11-24 22:35:28 -0300

    Piano virtual.

---
+++ src/scripts/engine.js
@@ -0,0 +1,43 @@
+const volume = document.querySelector("input#volume");
+const showedKeys = document.querySelector("input#showedKeys");
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+let audio = new Audio();
+const mapedKeys = [];
+
+pianoKeys.forEach((key) => {
+  key.addEventListener("click", () => {
+    playTune(key.dataset.key);
+  });
+  mapedKeys.push(key.dataset.key);
+});
+
+let playTune = (key) => {
+  audio.volume = volume.value / 100;
+  audio.src = `src/tunes/${key}.wav`;
+  console.log(audio.src);
+  audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
+};
+
+document.addEventListener("keydown", (e) => {
+  if (mapedKeys.includes(e.key)) playTune(e.key);
+});
+
+showedKeys.addEventListener("change", (e) => {
+  setTimeout(() => {
+    showHideKeys(e);
+  }, 300);
+});
+
+function showHideKeys(event) {
+  pianoKeys.forEach((key) => {
+    event.target.checked
+      ? (key.textContent = key.dataset.key)
+      : (key.textContent = "");
+  });
+}
